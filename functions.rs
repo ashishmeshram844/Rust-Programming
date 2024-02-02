3c8908ef072b82b46fab66f9f9ac734756102c5d
Ashish <ashish.m@netwebindia.com>
2024-02-02 16:29:39 +0530

    added some extra inbuits functions on string in functions

--- functions.rs
+++ functions.rs
@@ -8,7 +8,21 @@ fn add_nums(num1:i64, num2:i64){
     println!("{} + {}  = {}",num1,num2,sum);
 }
 
+
+
 fn main(){
     print_name();
-    add_nums(5,10)
+    add_nums(5,10);
+
+    fn convert_lower(string: &str){
+        let _new_str  = string.to_ascii_lowercase();
+        println!("{}",_new_str);
+        println!("{}",string)
+    }
+    fn find_len(string : &str){
+        println!("{}",string.len())
+    }
+
+    convert_lower("ASHISH");
+    find_len("ASHISH");
 }
