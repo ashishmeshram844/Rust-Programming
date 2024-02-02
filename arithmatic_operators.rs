5b93faa24a8eb931f4e2696c331c7ab7568b5836
Ashish <ashish.m@netwebindia.com>
2024-02-02 13:56:31 +0530

    arithmatic operators

---
+++ arithmatic_operators.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let num1:u16 = 124;
+    let num2:u16 = 5;
+    println!("Addition of {} and {} is {}",num1,num2,num1+num2);
+    println!("Substraction of {} and {} is {}",num1,num2,num1-num2);
+    println!("Multiplication of {} and {} is {}",num1,num2,num1*num2);
+    println!("Division of {} and {} is {}",num1,num2,num1/num2);
+}
