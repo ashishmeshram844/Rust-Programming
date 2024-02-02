ed9cca73605116048ccb53e1adb14ee20c515c67
Ashish <ashish.m@netwebindia.com>
2024-02-02 15:05:34 +0530

    conditional statements

---
+++ conditional_statements.rs
@@ -0,0 +1,15 @@
+fn main(){
+    let _x = 90;
+
+    if _x <= 20{
+        println!("You inputed less than 20");
+    }
+    else if _x == 90{
+        println!("You inputed 90");
+    }
+    else{
+        println!("You inputed greater than 20");
+    }
+}
+
+
