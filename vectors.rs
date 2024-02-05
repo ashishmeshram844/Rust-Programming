003b97d0a9256f24f5b6779c2b229f7147fe5940
Ashish <ashish.m@netwebindia.com>
2024-02-05 16:11:56 +0530

    vectors implemented

---
+++ vectors.rs
@@ -0,0 +1,13 @@
+ fn main(){
+    let mut my_vec = vec![1,2,3];           // initializing a vector
+    println!("{}",my_vec[0]);
+
+    my_vec.push(49);        //add extra item in vector at last position
+    println!("{}",my_vec[my_vec.len()-1]);      
+    println!("{:?}",my_vec);        //prints while vector
+
+    my_vec.remove(0);           // remove index value
+    println!("{:?}",my_vec);
+
+
+ }
